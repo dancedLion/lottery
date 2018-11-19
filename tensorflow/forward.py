b452abca07b668fe4d610e2466ea4f7cd8502cdb
dotbyte <dotbyte@163.com>
2018-11-19 09:55:28 +0800

    do test

--- tensorflow/forward.py
+++ tensorflow/forward.py
@@ -2,3 +2,16 @@ import tensorflow as tf
 import numpy as np
 
 
+'''
+计算概率
+影响因素:
+1-每一次的初始化概率
+2-数的距离
+3-相互数的概率
+    4-数的顺序
+
+'''
+
+fl=[i+1 for i in range(35)]
+bl=[i+1 for i in range(15)]
+
--- tensorflow/output.py
+++ tensorflow/output.py
@@ -1,1 +1,6 @@
 from matplotlib import pyplot
+
+'''
+图形化显示每个数的概率
+
+'''
